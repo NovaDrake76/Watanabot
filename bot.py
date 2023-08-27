8938dcd3f581c082353894ef33c6479c98d89591
NovaDrake76 <novadrake76@gmail.com>
2023-08-26 22:54:40 -0300

    fix aws lol

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ import boto3
 s3 = boto3.client('s3')
 
 session = boto3.Session(
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
     region_name='South America (Sao Paulo)'
 )
 
